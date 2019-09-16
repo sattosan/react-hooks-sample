a98e00d5500765a4206ef90cce8a4228c0d953ad
sattosan <sato.yuya1211@gmail.com>
2019-09-16 17:13:02 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+/*
+action = {
+  type: 'CREATE_EVENT',
+  title: '2020東京オリンピックのお知らせ',
+  body: '2020年に東京オリンピックを開催します！つきましては...'
+}
+
+before
+state = []
+
+after
+state = [
+  {id: 1, title: 'タイトル1', body: 'ボディー1'}
+]
+*/
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id: id, ...event }];
+    case "DELETE_EVENT":
+      return state;
+    case "DELETE_ALL_EVENTS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
